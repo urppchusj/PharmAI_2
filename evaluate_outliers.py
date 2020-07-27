d0c7e79b9920613e59f5d3fcb4544dacf88e2bba
Maxime Thibault <maximethibault1@gmail.com>
2020-07-26 20:32:59 -0400

    make a dataframe with the results

--- evaluate_outliers.py
+++ evaluate_outliers.py
@@ -5,4 +5,6 @@ import pandas as pd
 results = pd.read_csv('model/cv_results.csv')
 
 # %%
-results[['param_anomaly_algorithm','param_tsvd__num_topics', 'mean_test_Ratio anomalies Overall', 'std_test_Ratio anomalies Overall', 'mean_test_Ratio anomalies Oncologie', 'std_test_Ratio anomalies Oncologie','mean_test_Ratio anomalies Ob/gyn', 'std_test_Ratio anomalies Ob/gyn','mean_test_Ratio anomalies Pédiatrie', 'std_test_Ratio anomalies Pédiatrie','mean_test_Ratio anomalies Néonatologie', 'std_test_Ratio anomalies Néonatologie']]
+results[['param_anomaly_algorithm','param_tsvd__n_components', 'mean_test_Ratio anomalies Overall', 'std_test_Ratio anomalies Overall', 'mean_test_Ratio anomalies Oncologie', 'std_test_Ratio anomalies Oncologie','mean_test_Ratio anomalies Ob/gyn', 'std_test_Ratio anomalies Ob/gyn','mean_test_Ratio anomalies Pédiatrie', 'std_test_Ratio anomalies Pédiatrie','mean_test_Ratio anomalies Néonatologie', 'std_test_Ratio anomalies Néonatologie']]
+
+# %%
